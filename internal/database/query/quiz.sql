c0735d6303fcafe7c0f74deddc3ea3add15977cd
ali <ari02rahimi@gmail.com>
2024-02-26 15:42:01 +0330

    initial2.0

--- internal/database/query/quiz.sql
+++ internal/database/query/quiz.sql
@@ -1,5 +1,6 @@
 -- name: CreateQuiz :one
 INSERT INTO "quiz" (
   user_id, title, question_no, start_at, end_at, questions
-) VALUES ($1, $2, $3, $4, $5, $6)
+) VALUES 
+($1, $2, $3, $4, $5, $6)
 RETURNING id;
