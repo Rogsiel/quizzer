c9e749b851d6b6db0add95a5bd7753f48ecddd37
ali <ari02rahimi@gmail.com>
2024-04-11 16:31:44 +0330

    feat(service): add handlers for Signup and login

--- internal/service/server/account_handler.go
+++ internal/service/server/account_handler.go
@@ -3,14 +3,32 @@ package server
 import (
 	"database/sql"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rogsiel/quizzer/internal/auth"
 	db "github.com/rogsiel/quizzer/internal/database"
 )
 
 type createAccountReq struct {
-    Name    string  `json:"name" binding:"required"`
-    Email   string  `json:"email" binding:"required,email"`
+    UserName	string  `json:"user_name" binding:"required,alphanum"`
+    Email	string  `json:"email" binding:"required,email"`
+    Password	string	`json:"password" binding:"required,min=8"`
+}
+type userResponse struct {
+    UserName		string	    `json:"user_name"`
+    Email		string	    `json:"email"`
+    PasswrordChangedAt	time.Time   `json:"password_changed_at"`
+    CreatedAt		time.Time   `json:"created_at"`
+}
+
+func newUserResponse(user db.User) userResponse{
+    return userResponse{
+	UserName: user.UserName,
+	Email: user.Email,
+	PasswrordChangedAt: user.PasswordChangedAt,
+	CreatedAt: user.CreatedAt,
+    }
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
@@ -19,44 +37,74 @@ func (server *Server) createAccount(ctx *gin.Context) {
         ctx.JSON(http.StatusBadRequest, errorResponse(err))
 	return
     }
-    arg := db.SignupTxParams{
-	Name: req.Name,
+    
+    hashedPassword, err := auth.HashPassword(req.Password)
+    if err != nil {
+	ctx.JSON(http.StatusBadRequest, errorResponse(err)) 
+	return
+    }
+    arg := db.CreateUserParams{
+	UserName: req.UserName,
 	Email: req.Email,
+	HashedPassword: hashedPassword,	
     } 
 
-    account, err := server.store.SignupTx(ctx, arg)
+    account, err := server.store.CreateUser(ctx, arg)
     if err != nil {
 	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 	return
     }
-
-    ctx.JSON(http.StatusOK, account)
+    
+    res := newUserResponse(account)
+    ctx.JSON(http.StatusOK, res)
 }
 
-type getUserQuizListReq struct{
-    ID    int64  `uri:"id" binding:"required"`
+type userLoginReq struct {
+    UserName	string  `json:"user_name" binding:"required,alphanum"`
+    Password	string	`json:"password" binding:"required,min=8"`
 }
 
-func (server *Server) getUserQuizList(ctx *gin.Context) {
-    var req getUserQuizListReq
-    if err := ctx.ShouldBindUri(&req); err != nil {
-	ctx.JSON(http.StatusBadRequest, errorResponse(err))
+type userLoginRes struct {
+    AccessToken	string		`json:"access_token"`
+    User	userResponse    `json:"user"`
+}
+
+func (server *Server) UserLogin(ctx *gin.Context) {
+    var req userLoginReq
+    if err := ctx.ShouldBindJSON(&req); err != nil {
+    	ctx.JSON(http.StatusBadRequest, errorResponse(err))
 	return
-    }
-    arg := db.GetUserQuizTxParams{
-	ID: req.ID,
-    }
-
-    userQuizList, err := server.store.GetUserQuizTx(ctx,arg)
+    } 
+    
+    user, err := server.store.GetUser(ctx, req.UserName)
     if err != nil {
 	if err == sql.ErrNoRows {
 	    ctx.JSON(http.StatusNotFound, errorResponse(err))
 	    return
 	}
-
 	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 	return
     }
 
-    ctx.JSON(http.StatusOK, userQuizList)
+    err = auth.CheckPassword(req.Password, user.HashedPassword)
+    if err != nil {
+	ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+	return
+    }
+
+    accessToken, err := server.tokenMaker.CreateToken(
+	user.UserName,
+	server.config.AccessTokenDuration,
+    )
+    if err != nil {
+	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	return
+    }
+
+    res := userLoginRes{
+	AccessToken: accessToken,
+	User: newUserResponse(user),
+    }
+
+    ctx.JSON(http.StatusOK, res)
 }
