096fa86949bf6aba385288a547c816fe93b44e76
ali <ari02rahimi@gmail.com>
2024-04-11 16:30:00 +0330

    feat(service): add a route.go file for routing in server and adding authorization

--- internal/service/server/server.go
+++ internal/service/server/server.go
@@ -1,25 +1,34 @@
 package server
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rogsiel/quizzer/config"
+	"github.com/rogsiel/quizzer/internal/auth/token"
 	db "github.com/rogsiel/quizzer/internal/database"
 )
 
 type Server struct {
-    store *db.Store
-    router *gin.Engine
+    store	db.Store
+    tokenMaker	token.Maker	
+    router	*gin.Engine
+    config	config.Config
 }
 
-func NewServer(store *db.Store) *Server {
-    server := &Server{store: store}
-    router := gin.Default()
+func NewServer(config config.Config,store db.Store) (*Server, error) {
+    tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+    if err != nil {
+	return nil, fmt.Errorf("can't initiate token maker: %w", err)
+    }
     
-    router.POST("/signup", server.createAccount)
-    router.GET("/search/:id",server.getUserQuizList)
-    router.POST("/quiz/new", server.createQuiz)
-    router.GET("/quiz/:id", server.getQuizByID)
-    server.router = router
-    return server
+    server := &Server{
+	store: store,
+	tokenMaker: tokenMaker,
+	config: config,
+    }
+    server.setRouter()  
+    return server, nil
 }
 
 func (server *Server) Start(address string) error {
