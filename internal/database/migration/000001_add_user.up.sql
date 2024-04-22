9e90c0b2ac671775a8cd8972bcb7bf506883d92e
ali <ari02rahimi@gmail.com>
2024-04-22 17:13:47 +0330

    feat(database): better migration versioning and add otp table

--- internal/database/migration/000001_add_user.up.sql
+++ internal/database/migration/000001_add_user.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "user" (
   "id" bigserial PRIMARY KEY NOT NULL,
   "user_name" varchar UNIQUE NOT NULL,
-  "email" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
   "is_email_verified" boolean NOT NULL DEFAULT false,
   "hashed_password" varchar NOT NULL,
   "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
--- internal/database/migration/000005_verify_email.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS "verify_email" CASCADE;
