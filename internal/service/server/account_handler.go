028431c6e81bb6dfa980aeaf8d93ef1b0cada209
ali <ari02rahimi@gmail.com>
2024-04-24 19:50:31 +0330

    feat(service/server): add routes and handlers for password reset

--- internal/service/server/account_handler.go
+++ internal/service/server/account_handler.go
@@ -72,11 +72,15 @@ func (server *Server) createAccount(ctx *gin.Context) {
     }
 
     sender := mail.NewEmail()
-    sender.SendWelcomeEmail(mail.NewUserInfo{
+    err = sender.SendWelcomeEmail(mail.NewUserInfo{
 	UserName: account.UserName,
 	Email: account.Email,
 	OtpCode: otp.OtpCode,
     })
+    if err != nil {
+	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	return
+    }
 
     res := newUserResponse(account)
     ctx.JSON(http.StatusOK, res)
@@ -165,25 +169,10 @@ func (server *Server) UserLogin(ctx *gin.Context) {
     ctx.JSON(http.StatusOK, res)
 }
 
-type emailVerifyReq struct {
-    Email   string  `uri:"email" binding:"required"`
-    OtpCode string  `uri:"otp_code" binding:"required"`
-    OtpType string  `uri:"otp_type" binding:"required"`
-}
-
 func (server *Server) emailVerify(ctx *gin.Context) {
-    var req emailVerifyReq
-    if err := ctx.ShouldBindUri(&req); err != nil {
-    	ctx.JSON(http.StatusBadRequest, errorResponse(err))
-	return
-    }
-    record, err := server.store.GetOTP(ctx, db.GetOTPParams{
-	Email: req.Email,
-	OtpCode: req.OtpCode,
-	OtpType: req.OtpType,
-    })
+    record, err := server.store.GetOTP(ctx, ctx.Query("otp_code"))
     if err != nil {
-	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	ctx.JSON(http.StatusNotFound, errorResponse(err))
 	return
     }
     otp := otp.OTP{
@@ -196,7 +185,7 @@ func (server *Server) emailVerify(ctx *gin.Context) {
 	ExpiredAt: record.ExpiredAt,
     }
     
-    err = otp.VerifyOTP()
+    err = otp.VerifyOTP(ctx.Query("otp_type"))
     if err != nil {
 	ctx.JSON(http.StatusBadRequest, errorResponse(err))
 	return
@@ -205,7 +194,6 @@ func (server *Server) emailVerify(ctx *gin.Context) {
     username, err := server.store.VerifyEmailTx(ctx, db.VerifyEmailTxParams{
 	OtpID: otp.ID,
 	Email: otp.Email,
-	OtpCode: otp.OtpCode,
     })
     if err != nil {
 	ctx.JSON(http.StatusBadRequest, errorResponse(err))
@@ -213,4 +201,95 @@ func (server *Server) emailVerify(ctx *gin.Context) {
     }
     
     ctx.JSON(http.StatusOK, username)
+}
+
+type forgotPasswordReq struct {
+    UserName  string    `json:"user_name" binding:"required"`
+}
+
+func (server *Server) forgotPassword(ctx *gin.Context) {
+    var req forgotPasswordReq
+    if err := ctx.ShouldBindJSON(&req); err != nil {
+    	ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	return
+    }
+    
+    user, err := server.store.GetUser(ctx, req.UserName)
+    if err != nil {
+	ctx.JSON(http.StatusNotFound, errorResponse(err))
+	return
+    }
+    
+    otp := server.otpManager.NewPasswordChangeOTP(user.Email)
+    err = server.store.CreateOTPTx(ctx, db.CreateOTPTxParams{
+	Email: otp.Email,
+	OtpCode: otp.OtpCode,
+	OtpType: otp.OtpType,
+    })
+    if err != nil {
+	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	return
+    }
+
+    sender := mail.NewEmail()
+    err = sender.SendPasswordResetEmail(mail.ResetPasswordInfo{
+	UserName: req.UserName,
+	Email: otp.Email,
+	OtpCode: otp.OtpCode,
+    })
+    if err != nil {
+	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	return
+    }
+    
+    ctx.JSON(http.StatusOK, gin.H{"message":"Password Reset email sent"}) 
+}
+
+type resetPasswordReq struct {
+    NewPassword	string	`json:"new_password" binding:"required,min=8"`
+}
+
+func (server *Server) resetPassword(ctx *gin.Context) {
+    var req resetPasswordReq
+    if err := ctx.ShouldBindJSON(&req); err != nil {
+	ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	return
+    }
+    record, err := server.store.GetOTP(ctx, ctx.Query("otp_code"))
+    if err != nil {
+	ctx.JSON(http.StatusNotFound, errorResponse(err))
+    }
+ 
+    otp := otp.OTP{
+	ID: record.ID,
+	Email: record.Email,
+	OtpCode: record.OtpCode,
+	OtpType: record.OtpType,
+	IsUsed: record.IsUsed,
+	CreatedAt: record.CreatedAt,
+	ExpiredAt: record.ExpiredAt,
+    }
+    err = otp.VerifyOTP(ctx.Query("otp_type"))
+    if err != nil {
+	ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	return
+    }
+    
+    hashedPassword, err := auth.HashPassword(req.NewPassword)
+    if err != nil {
+	ctx.JSON(http.StatusBadRequest, errorResponse(err)) 
+	return
+    }
+
+    err = server.store.UpdatePasswordTx(ctx, db.UpdatePasswordTxParams{
+	OtpID: otp.ID,
+	Email: otp.Email,
+	HashedPassword: hashedPassword,
+    })
+    if err != nil {
+	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	return
+    }
+   
+    ctx.JSON(http.StatusOK, gin.H{"message":"Password changed succssfully"})
 }
